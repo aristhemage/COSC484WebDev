6496d928308929718e7c4c9843f806acdcabcedb
Riley Herritt <rileyherritt06@gmail.com>
2026-04-28 19:15:04 -0400

    initialized react frontend

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+/**This file exports the pages together so the app.jsx can import them as a group */
+export {Home} from './Home'
+export {About} from './About'
+export {CommunityGuidelines} from './CommunityGuidelines'
+export {Login} from './Login'
+export {Profile} from './Profile'
+export {SignUp} from './SignUp'
