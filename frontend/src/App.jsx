f01233c80017f58f5557091e149c68205c1a11f6
Riley Herritt <rileyherritt06@gmail.com>
2026-04-29 19:01:35 -0400

    added navbar to react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import {About, CommunityGuidelines, Home, Login, SignUp, Profile} from './pages'
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Outlet} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <BrowserRouter>
-      <h1>Hello</h1>
+      <Routes>
+        <Route element = {<MainLayout/>}>
+          <Route path = "/" element={<Home/>}/>
+          <Route path = "/about" element={<About/>}/>
+          <Route path = "/community-guidelines" element={<CommunityGuidelines/>}/>
+          <Route path = "/profile" element={<Profile/>}/>
+        </Route>
+        
+        <Route path = "/login" element={<Login/>}/>
+        <Route path = "/sign-up" element={<SignUp/>}/>
+        
+      </Routes>
     </BrowserRouter>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-function Navbar() {
+const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+          <nav className = "navbar">
+            <NavLink to = "/" id = "petstagram-logo">Petstagram</NavLink>
+            <ul>
+                  <li><NavLink to = "/" end>Home</NavLink></li>
+                  <li><NavLink to = "/about">About</NavLink></li>
+                  <li><NavLink to = "/community-guidelines">Community Guidelines</NavLink></li>
+                  <li><NavLink to = "/sign-up">Sign up</NavLink></li>
+                  <li><NavLink to = "/login">Login</NavLink></li>
+                  <li><NavLink to = "/profile">Profile</NavLink></li>
+            </ul>
+          </nav>
   )
 }
 
